4d14e57ed7f33e6a252d14f056f9051f9209e975
developmaxcode <developmaxcode@gmail.com>
2021-05-28 13:54:10 -0500

    start time

--- src/commands.py
+++ src/commands.py
@@ -18,6 +18,14 @@ async def carr(ctx):
 
 
 @bot.command()
+async def prefix(ctx):
+    print("+---------------------+")
+    print("iniciado con exito")
+    bot = commands.Bot(command_prefix="<")
+    print("termino con exito")
+
+
+@bot.command()
 async def nuke(ctx, ready=""):
     if ready == "stop":
         await ctx.send("comando nuclear detenido")
@@ -44,37 +52,40 @@ async def tim(ctx):
 @bot.command(aliases=["cls"])
 @commands.has_permissions(manage_messages=True)
 @commands.bot_has_permissions(manage_messages=True)
-async def clear(ctx, m=None):
+async def clear(ctx, num_messages=None):
+    print("+----------------------------+")
     global d, embed
-    if m is None:
+    if num_messages is None:
         embes = discord.Embed(description="<a:no:846822577666392094> Requiere un argumento <a:no:846822577666392094> ",
                               color=discord.Color.red())
         await ctx.send(embed=embes)
         return
-    elif m.isnumeric():
-        m = int(m)
+    elif num_messages.isnumeric():
+        num_messages = int(num_messages)
         d = []
-        if m == 0:
+        print("esto no deberia verse")
+        if num_messages == 0:
             embed = discord.Embed(title="Aviso", description="{0} mensajes borrados".format(len(d)),
                                   color=discord.Color.red())
             await ctx.send(embed=embed, delete_after=4)
             return
     else:
+        print("esto si deberia verse")
         await ctx.send("Solo argumentos numericos positivos")
         return
     await ctx.message.delete()
     f = datetime.utcnow() - timedelta(days=14)
-    async for message in ctx.channel.history(limit=m):
+    async for message in ctx.channel.history(limit=num_messages):
         time = message.created_at
-        if f <= time <= datetime.utcnow() and m >= 1:
-            d = await ctx.channel.purge(limit=m, after=datetime.utcnow() - timedelta(days=14))
+        if f <= time <= datetime.utcnow() and num_messages >= 1:
+            d = await ctx.channel.purge(limit=num_messages, after=datetime.utcnow() - timedelta(days=14))
             if len(d) > 1:
                 embed = discord.Embed(title="Aviso", description="{0} mensajes borrados".format(len(d)),
                                       color=discord.Color.red())
             elif len(d) in range(0, 2):
                 embed = discord.Embed(title="Aviso", description="{0} mensaje borrado".format(len(d)),
                                       color=discord.Color.red())
-            m = 0
+            num_messages = 0
         elif not f <= time <= datetime.utcnow():
             if len(d) >= 1:
                 await ctx.send(
@@ -104,5 +115,6 @@ async def clear_error(ctx, error):
                              color=discord.Color.purple())
         await ctx.send(embed=embe)
 
-    elif isinstance(error, commands.errors.ExpectedClosingQuoteError):
+    elif isinstance(error, commands.errors.ExpectedClosingQuoteError) or isinstance(error,
+                                                                                    commands.errors.UnexpectedQuoteError):
         await ctx.send("Solo argumentos numericos positivos")
--- src/events.py
+++ src/events.py
@@ -1,3 +1,5 @@
+import datetime
+
 import discord
 from discord.ext import commands, tasks
 from prefix import bot
@@ -6,16 +8,17 @@ status = [discord.Activity(type=discord.ActivityType.watching, name=f"Mi prefix 
           discord.Streaming(name="ROLON",
                             url="https://www.youtube.com/watch?v=CLaJp3JnpyQ&list=RDrFk87rdyPro&index=17")]
 y, i = 0, 1
-
+a = "$"
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=""))
     status_change.start()
-    print(f"bot encendido: {bot.user.name}")
-    print(f"bot id: {bot.user.id}")
-    print(f"bot servers: {len(bot.guilds)}")
+    print(f"Bot encendido: {bot.user.name}")
+    print(f"Bot id: {bot.user.id}")
+    print(f"Bot servers: {len(bot.guilds)}")
     print('Name_server:', ','.join([str(lst) for lst in bot.guilds]))
+    print(f"Fecha de inicio: {datetime.datetime.now().strftime('%d-%B-%Y %I:%M:%S %p')}")
 
 
 @tasks.loop(seconds=10)
@@ -32,6 +35,9 @@ async def status_change():
 @bot.event
 async def on_message(message):
     if bot.user.mentioned_in(message):
+        global a
+        a = bot.user.mentioned_in(message)
+        print(a)
         await message.channel.send(f"Mi prefix es {bot.command_prefix}")
     await bot.process_commands(message)
 
